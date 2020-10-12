4debd8730d1edeac37c2e5a629ce178b045e2e4a
Kirubaharan Palani <pkiruba85@gmail.com>
2020-10-12 21:30:55 +0530

    updated the content based on feedback

--- habitfit/js/main.js
+++ habitfit/js/main.js
@@ -272,6 +272,22 @@ AOS.init({
         }
     });
 
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+
+        closeBtnInside: true,
+        preloader: false,
+
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
     // $('.appointment_date').datepicker({
     // 	'format': 'm/d/yyyy',
     // 	'autoclose': true
